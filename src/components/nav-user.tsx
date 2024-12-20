6e9387c28ab7fa9924734103b5699bb931536db2
rabunthoeun7777 <rabunthoeun7777@gmail.com>
2024-12-20 11:47:56 +0700

    update

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -2,18 +2,18 @@
 
 import {
   BadgeCheck,
-  Bell,
+  // Bell,
   ChevronsUpDown,
-  CreditCard,
+  // CreditCard,
   LogOut,
   Sparkles,
 } from "lucide-react"
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
+// import {
+//   Avatar,
+//   AvatarFallback,
+//   AvatarImage,
+// } from "@/components/ui/avatar"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -66,10 +66,10 @@ export function NavUser() {
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg">
+              {/* <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.avatar} alt={`${user.firstName} ${user.lastName}`} />
-                {/* <AvatarFallback className="rounded-lg">CN</AvatarFallback> */}
-              </Avatar>
+                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              </Avatar> */}
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{`${user.firstName} ${user.lastName}`}</span>
                 <span className="truncate text-xs">{user.email}</span>
@@ -84,11 +84,11 @@ export function NavUser() {
             sideOffset={4}
           >
             <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
+              <div className="flex items-center gap-2 px-4 py-1.5 text-left text-sm">
+                {/* <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.avatar} alt={`${user.firstName} ${user.lastName}`} />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                </Avatar>
+                </Avatar> */}
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">{`${user.firstName} ${user.lastName}`}</span>
                   <span className="truncate text-xs">{user.email}</span>
@@ -105,19 +105,19 @@ export function NavUser() {
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
-                <Link href="/profile" className="flex gap-2">
+                <Link href="/dashboard/profile" className="flex gap-2">
                   <BadgeCheck />
                   Account
                 </Link>
               </DropdownMenuItem>
-              <DropdownMenuItem>
+              {/* <DropdownMenuItem>
                 <CreditCard />
                 Billing
               </DropdownMenuItem>
               <DropdownMenuItem>
                 <Bell />
                 Notifications
-              </DropdownMenuItem>
+              </DropdownMenuItem> */}
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuItem onClick={logout}>
